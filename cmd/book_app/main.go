63fe452aea5edf92621cc311abda781aefac116e
caohoangphuctd97 <phuc@ionmobility.com>
2024-03-13 09:49:28 +0700

    add routes

--- cmd/book_app/main.go
+++ cmd/book_app/main.go
@@ -3,24 +3,23 @@ package main
 import (
 	"os"
 
-	"github.com/create-go-app/fiber-go-template/pkg/configs"
-	"github.com/create-go-app/fiber-go-template/pkg/middleware"
-	"github.com/create-go-app/fiber-go-template/pkg/routes"
+	"github.com/caohoangphuctd97/go-test/pkg/configs"
+	routes "github.com/caohoangphuctd97/go-test/pkg/routers"
 	"github.com/create-go-app/fiber-go-template/pkg/utils"
-
+	"github.com/gofiber/contrib/otelfiber"
 	"github.com/gofiber/fiber/v2"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 
 	_ "github.com/create-go-app/fiber-go-template/docs" // load API Docs files (Swagger)
 
 	_ "github.com/joho/godotenv/autoload" // load .env file automatically
 )
 
-// @title API
+// @title GO exercise #2
 // @version 1.0
-// @description This is an auto-generated API Docs.
+// @description This is a swagger for go exercise #2.
 // @termsOfService http://swagger.io/terms/
-// @contact.name API Support
-// @contact.email your@mail.com
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 // @BasePath /api
@@ -29,20 +28,25 @@ import (
 // @name Authorization
 func main() {
 	// Define Fiber config.
+	zerolog.TimeFieldFormat = zerolog.TimeFieldFormat
+
+	log.Info().Msg("Start server")
 	config := configs.FiberConfig()
 
 	// Define a new Fiber app with config.
 	app := fiber.New(config)
 
 	// Middlewares.
-	middleware.FiberMiddleware(app) // Register Fiber's middleware for app.
+	app.Use(otelfiber.Middleware())
 
 	// Routes.
-	routes.SwaggerRoute(app)  // Register a route for API Docs (Swagger).
-	routes.PublicRoutes(app)  // Register a public routes for app.
-	routes.PrivateRoutes(app) // Register a private routes for app.
-	routes.NotFoundRoute(app) // Register route for 404 Error.
+	routes.SwaggerRoute(app) // Register a swagger APIs
 
+	// routes.PublicRoutes(app)  // Register a public routes for app.
+	// routes.PrivateRoutes(app) // Register a private routes for app.
+	// routes.NotFoundRoute(app) // Register route for 404 Error.
+
+	app.Listen("0.0.0.0:8080")
 	// Start server (with or without graceful shutdown).
 	if os.Getenv("STAGE_STATUS") == "dev" {
 		utils.StartServer(app)
